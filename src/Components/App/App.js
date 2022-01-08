ea4c67509dbfd8219e852e978d215527d4a1fd62
aronkleinhans <kleinhans.aron@gmail.com>
2022-01-08 14:25:39 +0100

    up to step 11

---
+++ src/Components/App/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './Components/App/App.css';
+
+function App() {
+
+}
+
+ReactDOM.render(
+    <div>
+    <h1>Ja<span className="highlight">mmm</span>ing</h1>
+    <div className="App">
+      <!-- Add a SearchBar component -->
+      <div className="App-playlist">
+        <!-- Add a SearchResults component -->
+        <!-- Add a Playlist component -->
+      </div>
+    </div>
+    </div>,
+    document.getElementById('root')
+  );
+
+export default App;
